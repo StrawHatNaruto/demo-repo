75b1f5db798ac59074f4b56d74a9f04aac835544
aditya <kapadi119@gmail.com>
2026-03-05 03:50:12 +0530

    cehcking the new commit

--- index.js
+++ index.js
@@ -15,3 +15,20 @@ function add (a , b){
 
     return a + b;
 }
+const age = 24;
+const sub = 'adi';
+const name =  'aditya';
+
+
+function add (a , b){
+
+    return a + b;
+}
+function add (a , b){
+
+    console.log(aaaa);
+    
+    
+
+    return a + b;
+}
